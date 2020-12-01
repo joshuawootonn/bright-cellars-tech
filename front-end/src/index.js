5fc11e1c1823914005a569f7635f403a98be2522
joshuawootonn <joshuawootonn@gmail.com>
2020-12-01 08:27:27 -0600

    feat: theme update with overrides and color

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import App from './components/App';
 import './index.scss';
+import * as colors from './constants/colors';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: colors.EDEN,
+        },
+    },
+    overrides: {
+        MuiSelect: {
+            select: {
+                color: colors.WHITE,
+            },
+            icon: {
+                color: colors.WHITE,
+            },
+        },
+        MuiInput: {
+            underline: {
+                '&:before': {
+                    borderBottomColor: colors.WHITE,
+                },
+                '&:after': {
+                    borderBottomColor: colors.WHITE,
+                },
+            },
+        },
+    },
+});
 
 const renderApp = () => {
     ReactDOM.render(
